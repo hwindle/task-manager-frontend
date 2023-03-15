5b0a8f6f72e74914e042b0302f5f6055f2f9aaca
Hazel Windle <hwindle83@googlemail.com>
2023-03-15 11:31:12 +0000

    Added a title to the landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,22 @@ import Login from '../components/auth/Login/Login';
 
 const Landing = () => {
   return (
-    <div className='landing-page'>
-      
-      <Login />
-    </div>
+    <>
+      <h1
+        style={{
+          margin: 0,
+          textAlign: 'center',
+          color: 'white',
+          width: '100%',
+          padding: '1rem',
+          backgroundColor: 'rgba(0, 0, 0, 0.9)',
+        }}>
+        Task Manager
+      </h1>
+      <div className='landing-page'>
+        <Login />
+      </div>
+    </>
   );
 };
 
